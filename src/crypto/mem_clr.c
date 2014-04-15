6c67d19a71339f057459f282c20fb10276bab4fc
jsing <jsing>
2014-04-15 13:41:53 +0000

    First pass at applying KNF to the OpenSSL code, which almost makes it readable. This pass is whitespace only and can readily be verified using tr and md5.

--- src/crypto/mem_clr.c
+++ src/crypto/mem_clr.c
@@ -61,15 +61,17 @@
 
 unsigned char cleanse_ctr = 0;
 
-void OPENSSL_cleanse(void *ptr, size_t len)
-	{
+void
+OPENSSL_cleanse(void *ptr, size_t len)
+{
 	unsigned char *p = ptr;
-	size_t loop = len;
-	while(loop--)
-		{
-		*(p++) = cleanse_ctr;
-		cleanse_ctr += (17 + (unsigned char)((int)p & 0xF));
-		}
-	if(memchr(ptr, cleanse_ctr, len))
-		cleanse_ctr += 63;
+	size_t loop = len, ctr = cleanse_ctr;
+	while (loop--) {
+		*(p++) = (unsigned char)ctr;
+		ctr += (17 + ((size_t)p & 0xF));
 	}
+	p = memchr(ptr, (unsigned char)ctr, len);
+	if (p)
+		ctr += (63 + (size_t)p);
+	cleanse_ctr = (unsigned char)ctr;
+}
--- src/crypto/o_str.c
+++ src/crypto/o_str.c
@@ -60,17 +60,24 @@
 #include <e_os.h>
 #include "o_str.h"
 
-int OPENSSL_strncasecmp(const char *str1, const char *str2, size_t n)
-	{
+#if !defined(OPENSSL_IMPLEMENTS_strncasecmp) && \
+    !defined(OPENSSL_SYSNAME_WIN32) && \
+    !defined(NETWARE_CLIB)
+# include <strings.h>
+#endif
+
+int
+OPENSSL_strncasecmp(const char *str1, const char *str2, size_t n)
+{
 #if defined(OPENSSL_IMPLEMENTS_strncasecmp)
-	while (*str1 && *str2 && n)
-		{
+	while (*str1 && *str2 && n) {
 		int res = toupper(*str1) - toupper(*str2);
-		if (res) return res < 0 ? -1 : 1;
+		if (res)
+			return res < 0 ? -1 : 1;
 		str1++;
 		str2++;
 		n--;
-		}
+	}
 	if (n == 0)
 		return 0;
 	if (*str1)
@@ -84,13 +91,25 @@ int OPENSSL_strncasecmp(const char *str1, const char *str2, size_t n)
 	 * defined as well. */
 	return strncasecmp(str1, str2, n);
 #endif
-	}
-int OPENSSL_strcasecmp(const char *str1, const char *str2)
-	{
+}
+
+int
+OPENSSL_strcasecmp(const char *str1, const char *str2)
+{
 #if defined(OPENSSL_IMPLEMENTS_strncasecmp)
-	return OPENSSL_strncasecmp(str1, str2, (size_t)-1);
+	return OPENSSL_strncasecmp(str1, str2, (size_t) - 1);
 #else
 	return strcasecmp(str1, str2);
 #endif
-	}
+}
 
+int
+OPENSSL_memcmp(const void *v1, const void *v2, size_t n)
+{
+	const unsigned char *c1 = v1, *c2 = v2;
+	int ret = 0;
+
+	while (n && (ret = *c1 - *c2) == 0) n--, c1++, c2++;
+
+	return ret;
+}
